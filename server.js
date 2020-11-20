9f7447e068f36ddb1241d15ec26e40bb0d30ac0d
adnanshibli <adnannajem.24.95@gmail.com>
2020-11-20 13:44:51 +0200

    server

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { response } = require('express');
+// make a variable called express to use the library to build the server
+const express = require('express');
+// server has all proparties and method of express library
+const server = express();
+ const PORT =3500;
+
+ server.use(express.static('./public'))
+
+ server.get('/data',(request,response)=>{
+     response.send('hello');
+
+ })
+ server.get('/people',(request,response)=>{
+    let family =[
+        {name:'razan'},
+        {name:'adnan'},
+        {name:'amal'}
+    ];
+    response.send(family);
+
+})
+
+ server.listen(PORT,()=>{
+     console.log('hello');
+ })
